06796b020ea2060331b4d0c27356c8eec91a6c2a
starmindz <stanleynnamani3@gmail.com>
2024-09-09 07:59:22 +0100

    Add another port to localhost to prevent CORS error

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5000",
     "http://localhost:5173",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
     "https://healthhub-atbl.onrender.com",
     "https://abu-wears.netlify.app",
 ]
